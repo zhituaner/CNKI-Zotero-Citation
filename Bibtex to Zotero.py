5b1b99a96aee9a1b17f132c1ad74979ec3303489
zhituaner <95329679+zhituaner@users.noreply.github.com>
2022-03-26 19:27:17 +0800

    Add files via upload

---
+++ Bibtex to Zotero.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# coding: utf-8
+# 输入：从剪切板读取知网文献详情页URL；输出：生成该文献Bibtex并自动复制
+
+import re
+import os
+import time
+import pyperclip
+import pyautogui
+from pprint import pprint
+from zhconv import convert
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+
+start_bib=time.time()
+url=pyperclip.paste()
+print(f'文献详情页 {url}')
+try:
+    url_obj=re.compile(r'dbname=(.*?)&filename=(.*?)&',re.S)
+    url_result=url_obj.findall(url)
+    assert url_result !=[]
+    dbname,filename=url_result[0][0],url_result[0][1]
+except:
+    try:
+        url_obj=re.compile(r'filename=(.*?)&dbname=(.*?)&',re.S)
+        url_result=url_obj.findall(url)
+        assert url_result !=[]
+        dbname,filename=url_result[0][1],url_result[0][0]
+    except:
+        url_obj=re.compile(r'filename=(.*?)&dbcode=.*?&dbname=(.*?)&',re.S)
+        url_result=url_obj.findall(url)
+        assert url_result !=[]
+        dbname,filename=url_result[0][1],url_result[0][0]
+# print(url_result)
+assert url_result !=[]
+
+opt=Options()
+opt.add_argument('--headless')
+opt.add_argument('--disable-gpu')
+opt.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
+web_bib=Chrome(options=opt)
+# web_bib.maximize_window()
+
+url_info=f'https://kns.cnki.net/kns8/manage/export?filename={filename}&dbname={dbname}'
+print(f'文献信息页 {url_info}')
+web_bib.get(url_info)
+WebDriverWait(web_bib,15,0.5).until(lambda x:x.find_element(By.LINK_TEXT,'自定义'))
+web_bib.find_element(By.LINK_TEXT,'自定义').click()
+WebDriverWait(web_bib,15,0.5).until(lambda x:x.find_element(By.LINK_TEXT,'全选'))
+web_bib.find_element(By.LINK_TEXT,'全选').click()
+web_bib.find_element(By.LINK_TEXT,'预览').click()
+time.sleep(1)
+info=web_bib.find_element(By.CLASS_NAME,'literature-list').get_attribute('innerHTML')
+web_bib.quit()
+# pprint(info)
+
+info=convert(info,'zh-hans')
+try: # 题名
+    obj_title=re.compile(r'Title-题名:(.*?)<br>',re.S)
+    title=obj_title.findall(info)[0].replace(' ','')
+except:
+    title=''
+try: # 作者【注意多个作者的情况！】
+    obj_author=re.compile(r'Author-作者:(.*?)<br>',re.S)
+    author=obj_author.findall(info)[0].replace(' ','')
+    if author.count(';')<=1:
+        author=author.replace(';','')
+    else: # 多个作者：author应当形如 {{常玉芝} and {王曾瑜}}
+        templs=author.split(';')
+        author=''
+        for j in range(len(templs),-1,-1):
+            if j=='\n' or j=='':
+                del templs[j]
+        for i in templs:
+            i=i.replace(' ','')
+            i=i.replace('\n','')
+            author+=('{'+i+'} and ')
+        author=author[:-5]
+#     print(f'作者：author（用于检查）')
+except:
+    author=''
+try: # 文献来源（期刊名）
+    obj_source=re.compile(r'Source-文献来源:(.*?)<br>',re.S)
+    source=obj_source.findall(info)[0].replace(' ','')
+except:
+    source=''
+try: # 摘要
+    obj_summary=re.compile(r'Summary-摘要:(.*?)<br>',re.S)
+    summary=obj_summary.findall(info)[0].replace(' ','').replace('&lt;正&gt;','')
+except:
+    summary=''
+try: # 年
+    obj_year=re.compile(r'Year-年:(.*?)<br>',re.S)
+    year=obj_year.findall(info)[0].replace(' ','')
+except:
+    pubtime=''
+try: # 卷
+    obj_volume=re.compile(r'Volume-卷:(.*?)<br>',re.S)
+    volume=obj_volume.findall(info)[0].replace(' ','')
+except:
+    volume=''
+try: # 期
+    obj_period=re.compile(r'Period-期:(.*?)<br>',re.S)
+    period=obj_period.findall(info)[0].replace(' ','')
+except:
+    period=''
+try: # 页码
+    obj_pagecount=re.compile(r'PageCount-页码:(.*?)<br>',re.S)
+    pagecount=obj_pagecount.findall(info)[0].replace(' ','')
+except:
+    period=''
+
+# bibtex='''@article{title = {%s},author = {%s},year = {%s},journal = {%s},volume = {%s},number = {%s},\
+# pages = {%s},url = {%s},abstract = {%s}}''' %(title,author,year,source,volume,period,pagecount,url,summary)
+# 以上Bibtex有一定概率识别失败，可能为缩进原因
+bibtex='''@article{
+    title = {%s},
+    author = {%s},
+    year = {%s},
+    journal = {%s},
+    volume = {%s},
+    number = {%s},
+    pages = {%s},
+    url = {%s},
+    abstract = {%s}
+}''' %(title,author,year,source,volume,period,pagecount,url,summary)
+print(bibtex)
+os.system('D:/Softwares/Zotero/zotero.exe')
+pyperclip.copy(bibtex)
+time.sleep(0.5) # 应确保Zotero提前打开
+pyautogui.keyDown('ctrl'); pyautogui.keyDown('shift'); pyautogui.keyDown('alt'); pyautogui.keyDown('i')
+pyautogui.keyUp('i'); pyautogui.keyUp('alt'); pyautogui.keyUp('shift'); pyautogui.keyUp('ctrl') # 千万别忘了w
+end_bib=time.time()
+print(f'理论上已导入{author}：《{title}》，用时{round(end_bib-start_bib,2)}秒。（如未成功导入，请手动按快捷键Ctrl+Shift+Alt+I）')
---
+++ README.md
@@ -0,0 +1,45 @@
+# `Bibtex to Zotero.py`
+
+**适用于在知网文献详情页无法通过Zotero Connector导入Zotero的文献**（部分期刊存在此现象）。虽然有[油猴脚本](https://greasyfork.org/zh-CN/scripts/393305-%E7%9F%A5%E7%BD%91-%E5%8F%82%E8%80%83%E6%96%87%E7%8C%AE-bibtex)可实现将NodeExpress格式转为bibtex，但往往缺少信息，故编写此程序。
+
+使用Python库：re、os、time、pyperclip、pyautogui、pprint、convert、selenium。**运行前复制文献详情页URL（[例](https://kns.cnki.net/kcms/detail/detail.aspx?dbcode=CJFD&dbname=CJFD2005&filename=ZGSJ2005S1006&uniplatform=NZKPT&v=qCniU69B0sacew3JfcvC1dbAYKgsbW4k2_3U6v-heGugt6kJJEmibrds1mt_tkL6)），并打开Zotero目标文件夹即可**，程序将自动生成Bibtex、复制到剪切板并导入Zotero。
+
+# `CNKI to Zotero.py`
+
+## 注：此功能已由Zotero6.0Beta实现
+
+### 编写目的
+
+**将所需:white_check_mark:知网文献自动化、批量导入Zotero。**（“所需”指检索结果页中，复选框处于勾选状态√的文献；目前尚未发现将知网文献批量导入Zotero的方法）
+
+### 使用说明
+
+1. 使用Python第三方库：**os**、**sys**、**selenium**【核心】、**pyautogui**；
+2. **当前仅支持Chrome**；此处**不含selenium浏览器驱动**，请根据**浏览器版本**自行下载【[**Chrome**](http://chromedriver.storage.googleapis.com/index.html)；[**Firefox**](https://github.com/mozilla/geckodriver/releases/)；[**IE**](http://selenium-release.storage.googleapis.com/index.html)】，并将其放在**Python安装目录**下（或添加至**环境变量**）；
+3. 其他具体事项，在运行中使用`pyautogui.confirm`**弹窗提示**；
+4. 为使**Zotero Connector**完整识别文献信息，本程序在`SaveToZotero()`函数内使用较多`time.sleep`~~（事实上手动操作也得等啊）~~，以确保使用**“Save to Zotero(CNKI)”**选项导出（否则会导出网页）。对运行时间的优化主要集中于此。
+5. 你需要做的**只是**打开Zotero、手动检索、勾选所需文献，程序将检测所选☑文献并自动导入Zotero。**在所需文献较多时尤为省事**。~~（可以挂着程序先去干饭嘛）~~
+6. 经测试，**批量导出成功率接近100%**。少数异常情况下会导出为网页，尝试手动导出后基本上依然如此，推测问题应该不在程序。
+
+# `Customize Zotero Citation.py`
+
+## 编写目的
+
+Zotero可以在Word中非常方便地插入引文，这是基于styles文件夹下的`.csl`样式文件实现的。然而，引文样式多种多样，在某些情况下无法从网上找到相应的`.csl`文件，也缺乏修改已有`.csl`文件所需的知识储备。**故【基于Zotero导出的文献库】编写此程序，通过修改源码，我们可以自定义任意引文格式，从而更加灵活地实现随写随引。**
+
+## 使用说明
+
+1. 使用Python第三方库：**os**、**pandas**、**pyperclip**、**pyautogui**；
+2. 建议使用前先确认无法找到所需`.csl`文件，像`GB/T 7714-2015`这样常用的格式还是可以找到的；
+3. 导出Zotero文献库`我的文库.csv`，将其放在本文件夹内，或在源码中修改路径`path_in`；
+4. 【**核心：自定义引文样式**】位于源码第二部分中的`Citation()`函数，**可在注释提示下修改为所需样式**；
+5. 运行程序后将使用`pyautogui.prompt`接受输入（直接回车可退出程序），输入**文献标题**即可检索，并将生成的引文**自动复制到剪切板**。
+
+## 版本更新
+
+### v2.0（当前）
+
+1. **封装函数**：各司其职，易于修改；
+2. **优化检索**：（仍以标题检索）增加**查无结果**和**多个结果**时的处理机制，后者使用`pyautogui.confirm`弹窗供使用者选择文献作者；
+3. **增加注释**：集中于程序核心的`Citation()`函数，便于在其提示下按需修改源码；
+4. **优化输出**：主要为在打印引文前**增加打印存储文献信息的字典**；在程序运行初**增加打印可供检索该字典的键**。
